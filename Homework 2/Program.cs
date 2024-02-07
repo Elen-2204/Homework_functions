f3e05637034f129bd3e5744486766a6b3ae08cd3
Elen <elen.v.grigoryan@gmail.com>
2024-02-07 17:49:42 +0400

    Add project files.

---
+++ Homework 2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework_2
+{
+    class Program
+    {
+        //findng middle elements
+        public static List<int> Middle(List<int> nums)
+        {
+            List<int> middleElems = new List<int>();
+            int length = nums.Count;
+            if (length % 2 != 0)
+            {
+                middleElems.Add(nums[length / 2]);
+            }
+            else
+            {
+                middleElems.Add(nums[length / 2 - 1]);
+                middleElems.Add(nums[length / 2]);
+            }
+            return middleElems;
+        }
+
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 1, 2, 4, 5 };
+            List<int> middleElems = Middle(numbers);
+            Console.WriteLine("Middle Element(s):");
+            foreach (var element in middleElems)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
+
+/*Ունենք 12 ոսկե ձուլակտոր որոնցից մեկը կեղծ է. 3 կշռումով ինչպես պարզենք թե որն է կեղծը
+         
+         [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+        1. Բաժանել 3 մասի և կշռել
+            1[1,2,3,4], 2[5,6,7,8],  3[9,10,11,12]
+            1st operation 1 * 2
+            if( 1 == 2) Արել էինք լսարանում
+            else if( 1 > 2) 
+        {
+              => Կամ 1,2,3,4ից մեկը ծանր է, կամ 5,6,7,8ից մեկը թեթև
+             Բաժանենք 1-8ը 3 մասի։ 1[1,2,5] 2[3,4,6], 3[7,8]
+             2nd operation  1 * 2
+             if( 1 == 2) 3րդ քայլով կշռել 7, 8ը ընտել թեթևը
+             else if(1>2) 3րդ քայլով կշռել 1, 2ը ընտրել ծանրը, եթե =են ընտրել 6։
+             else if(1<2)3րդ քայլով կշռել 3, 4ը ընտրել ծանրը, եթե =են ընտրել 5։
+         }
+         else if( 1 < 2) անել նույն բանը ինչ արել ենք (1>2) դեպքի համար։
+         
+         Հ․Գ․ Խնդիրը լուծել եմ Գոռի հետ։) 
+         
+         */
---
+++ Max element function/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Max_element_function
+{
+   
+    class Program
+    {
+        public static int MaximumEl(List<int> list)
+        {
+            int max = list[0];
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] > max)
+                {
+                    max = list[i];
+                }
+            }
+            return max;
+        }
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>() { 1, 5, 7, 11, 5,78, 4, 10 };
+            int max = MaximumEl(list);
+            Console.WriteLine("Maximum Element: " + max);
+        }
+    }
+}
